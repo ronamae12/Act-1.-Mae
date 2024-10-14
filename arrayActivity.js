2b386008cd3c4591287c72a3c6d4bfdedc8baa33
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-10-14 22:02:17 +0800

    Initial Commit

---
+++ arrayActivity.js
@@ -0,0 +1,20 @@
+let favSingers = ['Billie Eilish', 'Belle Mariano', 'Ariana Grande'];
+
+
+console.log( favSingers[0]);
+
+
+console.log(favSingers);
+
+
+let favNumbers = [5, 10, 18, 9];
+
+
+console.log(favNumbers);
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log( mixedArr[0]); 
+console.log( mixedArr[1]); 
+console.log( mixedArr[2]); 
+console.log( mixedArr[3]); 
